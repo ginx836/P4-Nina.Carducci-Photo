98bcda29467f7344401b769d737c418b9c549bfc
ginx836 <141227582+ginx836@users.noreply.github.com>
2023-11-08 16:36:26 +0100

    Update styles.min.css + maugallery.min.js
    Modification des données structurées
    Mise à jour des fichiers minifiés

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -186,7 +186,7 @@
       gallery.append(`<div class="modal fade" id="${
         lightboxId ? lightboxId : "galleryLightbox"
       }" tabindex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-body">
                             ${
